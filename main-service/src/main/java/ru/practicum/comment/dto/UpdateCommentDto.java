5270daca907f1c07c6b8732a7d7c330555b3b7b1
MAXVvidik <maksimvvedenskij@gmail.com>
2023-01-13 17:41:21 +0400

    version 1

---
+++ main-service/src/main/java/ru/practicum/comment/dto/UpdateCommentDto.java
@@ -0,0 +1,15 @@
+package ru.practicum.comment.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class UpdateCommentDto {
+    @NotNull
+    @NotBlank
+    private String text;
+}
